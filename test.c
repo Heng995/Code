796b3dc398bfee0797166674a6d9a8eb0350c58a
Heng995 <beng890905@gmail.com>
2021-07-30 22:05:51 +0800

    20210730

--- test.c
+++ test.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-
-int main()
+#include <stdlib.h>
+int main (void)
 {
-    int n;
-    while (scanf("%d",&n) != EOF)
+    int i=1, sum=0;
+    while (sum<=100)
     {
-        if ((n%4==0) && (n%100!=0)||(n%400==0)){
-            printf("閏年");
-        } else {
-            printf("平年");
-        }
-        
+        sum += i;
+        printf("%2d=%2d\n", i, sum);
+        i++;
     }
+    printf("%d次", i-1);
     return 0;
 }
