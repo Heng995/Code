bb23c0c9eba4620a2a97bb586888849ec1df3fda
Heng995 <beng890905@gmail.com>
2021-08-04 00:53:56 +0800

    20210804

--- school/7_11.c
+++ school/7_11.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 int main (void)
 {
-    for(int i=1;i<=9;i++)
+    for(int i = 1;i<=6;i++)
     {
-        for(int j=1;j<=9;j++)
+        for(int j= 1;j<=i;j++)
         {
-            printf("%d*%d=%2d  ", i, j, i*j);
+            printf("*");
         }
         printf("\n");
     }
