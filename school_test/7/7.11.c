0ee7d03a41df0293631e2036d43de34db368c174
Heng995 <beng890905@gmail.com>
2021-08-06 00:05:25 +0800

    20210806

--- school_test/7/7.11.c
+++ school_test/7/7.11.c
@@ -3,7 +3,7 @@
 int main (void)
 {
     int i, j, sum;
-    for(i=2;i<1000;i++)
+    for(i=1;i<1000;i++)
     {
         sum=0;
         for(j=1;j<i;j++)
---
+++ school_test/7/7.13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (void)
+{
+    int n, sum = 0;
+    printf("2+4+6+..+n=?\n");
+    printf("n = ");
+    scanf("%d", n);
+
+    while
+    {
+        
+    }
+}
