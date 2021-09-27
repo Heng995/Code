47a03d5290673fce72961fa396b731035057dc20
Heng995 <beng890905@gmail.com>
2021-09-27 11:43:46 +0800

    2

--- school/3_01.c
+++ school/3_01.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 int main (void)
 {
-    int n1 = 1241;
+    int n1 = 1240;
     double n2 = 5.234;
     
-    printf("n1的平方為 %d\n", n1 * n1);
-    printf("n2的34563456EEE平方為 %f\n", n2 * n2);
+    printf("num1的平方為 %d\n", n1 * n1);
+    printf("num2的平方為 %f\n", n2 * n2);
     
     system ("pause");
     return 0;
