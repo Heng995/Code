dad36f225f02a474dcc23f0c2bf64a355e0e8128
Fat Cats <87945907+Heng995@users.noreply.github.com>
2021-10-18 14:22:46 +0800

    20211018

--- java/literal.java
+++ java/literal.java
@@ -2,7 +2,11 @@ public class literal
 {
     public static void main(String argv[])
     {
+        int a,b,c =10;
+        int i,j, byte b;
         int i=10,j=20,k=30,sum=i+j+k;
         System.out.println(sum);
+
+
     }
 }
