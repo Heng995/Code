eadba564bd9c882711baab3f9fb3cb138258b676
Heng995 <beng890905@gmail.com>
2021-10-22 02:25:29 +0800

    20211022

--- Python_hw/hw6.2.py
+++ Python_hw/hw6.2.py
@@ -1,9 +1,16 @@
-num = int(input('輸入一個數字'))
+def x(n):
+    t = 0
+    while n - 1:
+        if n % 2 == 0:
+            n = n / 2
+        elif n % 2!= 0:
+            n = n * 3 + 1
+        t += 1
+    return t
 
-while num >= 1:
-    if num%2 == 1:
-        num = num*3+1
-        print(num)
-    else:
-        num = num/2
-        print(num)
+a = [x(n) for n in range(10,101)]
+
+print('10 - 100之間的次數:')
+print(a) 
+
+print('最多為', max(a), '次')
